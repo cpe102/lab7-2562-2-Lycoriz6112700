c73fb2cc0e5845f06f1adbc9f691433bbe20f77f
lab302-33 <lab302-33@labeng.local>
2020-01-21 16:41:03 +0700

    Final

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,46 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream> //status = "ONE-NIGHT-STAND";
+#include<string>
+using namespace std;
+
+int main(){
+    int old, tall, asset;
+    string status="";
+cout << "How old are you?" << "\n:";
+cin >> old;
+if (old <= 20){
+    cout << "How tall are you?" << "\n:";
+    cin >> tall;
+    if (tall < 160){
+        status = "UNFRIEND";
+        cout << status;
+    }else if (tall >= 160 and tall < 175){
+        status = "FRIEND";
+        cout << status;
+    }else{
+        cout << "How much do you have?(million)" << "\n:";
+        cin >> asset;
+        if (asset > 69){
+            status = "MARRIED";
+            cout << status;
+        }else{
+            status = "ONE-NIGHT-STAND";
+            cout << status;
+        } 
+    }
+}else if (old < 30){
+    cout << "How much do you have?(million)" << "\n:";
+    cin >> asset;
+    if (asset > 10){
+      status = "BEST FRIEND";
+      cout << status;
+    }else{
+      status = "UNFRIEND";
+      cout << status;
+    }
+}else{
+    status = "UNFRIEND";
+    cout << status;
+}
+
+return 0;
+}
